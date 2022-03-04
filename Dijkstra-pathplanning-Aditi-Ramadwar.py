ba88cbbc61379878bd68e51f02556fcf0d8be6cb
Aditi <aditiramadwar@hotmail.com>
2022-03-03 22:53:04 -0500

    Added animation for the while search

--- Dijkstra-pathplanning-Aditi-Ramadwar.py
+++ Dijkstra-pathplanning-Aditi-Ramadwar.py
@@ -3,6 +3,7 @@ import heapq as hq
 import cv2
 import matplotlib.pyplot as plt
 from scipy import ndimage
+import copy
 # create obstacles with clearance
 def circle(x, y):
     circ_eq = ((x - 300) ** 2 + (y - 185) ** 2 - 40 * 40) < 0
@@ -31,7 +32,7 @@ def backtrack(image, parent, goal, start):
     while node != start:
         node = parent[node]
         final_path.append(node)
-        image[node[0], node[1]] = [0, 255, 0]
+        # image[node[0], node[1]] = [0, 255, 0]
     return final_path, image
 
 def get_children(parent_node, image):
@@ -101,7 +102,7 @@ def djk (image, start, goal):
             visited.append(curr_node)
             children = get_children(curr_node, image)
             for child, child_cost in children:
-                image[child[0], child[1]] = [255, 0, 0]
+                # image[child[0], child[1]] = [255, 0, 0]
                 if child not in visited:
                     # add cost with the child cost with the parent cost
                     cur_child_cost =  child_cost + cur_cost
@@ -124,8 +125,8 @@ def djk (image, start, goal):
                         hq.heapify(que)
 
                     if child == goal:
-                        return True, image, nodes_cost, parent_nodes
-    return False, image, nodes_cost, parent_nodes
+                        return True, image, nodes_cost, parent_nodes, visited
+    return False, image, nodes_cost, parent_nodes, visited
 
 # initialize grid 
 w = np.ones([400, 250, 3], dtype = np.uint8)
@@ -134,12 +135,13 @@ w = 255 * w
 # add obstacles on the grid
 for x_pos in range(w.shape[0]):
     for y_pos in range(w.shape[1]):
+        # check if point is in any of the obstacle space
         if (circle(x_pos, y_pos) or hexa(x_pos, y_pos) or quad(x_pos, y_pos)):
             w[x_pos, y_pos] = [0, 0, 160]
 
 # initialize start and goal 
 start = (0, 0)
-goal = (100, 100)
+goal = (10, 10)
 
 #check if goal not is not in obstacle space
 if w[goal[0], goal[1]][2] == 160:
@@ -147,12 +149,31 @@ if w[goal[0], goal[1]][2] == 160:
 else:
         print("Valid goal. Searching for shortest path...")
         # start searching for shortest path
-        flag, w, cost, parents = djk (w, start, goal)
+        flag, img, cost, parents, visited = djk (w, start, goal)
         if (flag):
                 print("Path Found. Backtracking...")
-                path, w = backtrack(w, parents, goal, start)
-                # print("Shortest Path: ", path)
+                shortest_path, img = backtrack(img, parents, goal, start)
+                print("Done")
 
-w = ndimage.rotate(w, 90)
-imgplot = plt.imshow(w)
-plt.show()
+                w = cv2.flip(w, 0)
+                w = cv2.flip(w, 1)
+                w = cv2.rotate(w, cv2.cv2.ROTATE_90_CLOCKWISE)
+                # animate nodes explored
+                for vis in visited:
+                        w[w.shape[0] - int(vis[0]) - 1, int(vis[1]), :] = [255, 0, 0]
+                        cv2.imshow("Grid", w)
+                        cv2.waitKey(1)
+                # show path
+                for node in shortest_path:
+                        w[w.shape[0]-int(node[0])-1, int(node[1]), :] = [0, 255, 0]
+                cv2.imshow("Grid", w)
+                cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# show final grid with shortest path
+# w = ndimage.rotate(w, 90)
+# imgplot = plt.imshow(w)
+# plt.show()
+
+
+    
